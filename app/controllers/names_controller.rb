a36b827e12688940b63c6b32e881f4a804e58460
Daniel Schepers <schepedw@gmail.com>
2015-03-17 10:21:50 -0500

    ready to go

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -6,4 +6,16 @@ class NamesController < ApplicationController
       }
     end
   end
+
+  def check_unique
+    file_location = Rails.root.join('tmp', 'taken.txt')
+    File.open(file_location, 'r') do |file|
+      contents = file.read
+      head(409) and return if contents.split("\n").include?(params[:name])
+    end
+    File.open(file_location, 'a') do |file|
+      file.puts(params[:name])
+      head(200) and return
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/names' => 'names#index'
+  post '/names' => 'names#check_unique'
 end
